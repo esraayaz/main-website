674230736fd04aac4aff0871cfb97947e2c99e9b
Esra Ayaz <essayaz@hotmail.com>
2025-07-13 01:20:09 +0200

    Navbar improved 🌷

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import { assets } from "@/assets/assets";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Navbar = () => {
+  const [isScroll, setIsScroll] = useState(false);
+
   const sideMenuRef = useRef();
 
   const openMenu = () => {
@@ -13,13 +15,31 @@ const Navbar = () => {
     sideMenuRef.current.style.transform = "translateX(16rem)";
   };
 
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (scrollY > 50) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    });
+  }, []);
+
   return (
     <div>
-      <nav className="w-full fixed px-4 py-8 lg:px-8 xl:px-[10%] flex items-center justify-between z-50">
+      <nav
+        className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-8 flex items-center justify-between z-50 ${
+          isScroll ? "bg-white/30 backdrop-blur-sm shadow-sm" : ""
+        }`}
+      >
         <a href="#top">
-          <Image src={assets.logo} alt="logo" className="w-28 cursor-pointer" />
+          <Image src={assets.logo} alt="logo" className="w-30 cursor-pointer" />
         </a>
-        <ul className="hidden md:flex items-center gap-20 lg:gap-25 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
+        <ul
+          className={`hidden md:flex items-center gap-20 lg:gap-25 rounded-full px-12 py-3 ${
+            isScroll ? "" : "bg-white/90 shadow-md"
+          }  mr-22`}
+        >
           <li>
             <a className="font-Ovo" href="#top">
               Home
@@ -37,20 +57,20 @@ const Navbar = () => {
           </li>
         </ul>
 
-        <div className="">
-          <button className="hidden md:flex">
+        <div className="flex items-center">
+          <button className="hidden md:flex cursor-pointer">
             <Image src={assets.moon_icon} alt="moon" className="w-6" />
           </button>
 
-          <button className="block md:hidden ml-3" onClick={openMenu}>
-            <Image src={assets.menu_black} alt="moon" className="w-6" />
+          <button className="block md:hidden" onClick={openMenu}>
+            <Image src={assets.menu_black} alt="menu-black" className="w-6" />
           </button>
         </div>
 
         {/*-- --------- Mobile Menu --------- --*/}
         <ul
           ref={sideMenuRef}
-          className="flex md:hidden flex-col gap-5 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-fuchsia-100 transition duration-50"
+          className="flex md:hidden flex-col gap-5 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-fuchsia-50 transition duration-500"
         >
           <div className="absolute right-6 top-6" onClick={closeMenu}>
             <Image
@@ -63,7 +83,7 @@ const Navbar = () => {
             <Image
               src={assets.moon_icon}
               alt="moon"
-              className="md:hidden w-6"
+              className="md:hidden w-6 cursor-pointer"
             />
           </button>
           <li>
@@ -72,7 +92,7 @@ const Navbar = () => {
             </a>
           </li>
           <li>
-            <a className="font-Ovo" onClick={closeMenu} href="#about">
+            <a className="font-Ovo" onClick={closeMenu} href="#About">
               🐣 About Me
             </a>
           </li>
