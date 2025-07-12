5363f79127c61416001915d60c8c6e7f2a468db1
Esra Ayaz <essayaz@hotmail.com>
2025-07-12 16:48:32 +0200

    Page file updated 🌷

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
-import Header from "@/Components/Header";
 import Navbar from "@/Components/Navbar";
+import Header from "@/Components/Header";
 
 export default function Home() {
   return (
