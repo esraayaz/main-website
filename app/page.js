aef3578dfe4a2af57ef88f0cd9753ee1495c9598
Esra Ayaz <essayaz@hotmail.com>
2025-07-13 01:19:33 +0200

    App improved 🌷

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 "use client";
 import Navbar from "@/Components/Navbar";
 import Header from "@/Components/Header";
+import About from "@/Components/About";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Header />
+      <About />
     </>
   );
 }
