ce77770405997d2d1e01afbb74ce9f19d31b51cb
Esra Ayaz <essayaz@hotmail.com>
2025-07-13 02:46:41 +0200

    App must go on! 🐦‍🔥

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 import Navbar from "@/Components/Navbar";
 import Header from "@/Components/Header";
 import About from "@/Components/About";
+import Projects from "@/Components/Projects";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar />
       <Header />
       <About />
+      <Projects />
     </>
   );
 }
