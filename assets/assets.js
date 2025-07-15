1faaa3caaf90f176f0304670d1153c2fd30db4a2
Esra Ayaz <essayaz@hotmail.com>
2025-07-15 17:27:37 +0200

    Assets changed 💻

--- assets/assets.js
+++ assets/assets.js
@@ -84,24 +84,3 @@ export const workData = [
     bgImage: "/work_6.png",
   },
 ];
-
-export const infoList = [
-  {
-    icon: assets.code_icon,
-    iconDark: assets.code_icon_dark,
-    title: "Languages",
-    description: "HTML, CSS, JavaScript ...",
-  },
-  {
-    icon: assets.edu_icon,
-    iconDark: assets.edu_icon_dark,
-    title: "Education",
-    description: "Coursera, Scrimba, FreeCodeCamp ...",
-  },
-  {
-    icon: assets.project_icon,
-    iconDark: assets.project_icon_dark,
-    title: "Projects",
-    description: "+5 projects",
-  },
-];
