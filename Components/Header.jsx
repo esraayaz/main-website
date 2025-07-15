881c907201d51324f85321c3066947820ad007e9
Esra Ayaz <essayaz@hotmail.com>
2025-07-15 17:27:17 +0200

    Project component changed 💻

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -10,18 +10,16 @@ const Header = () => {
       className="bg-radial-[at_50%_25%] from-white via-purple-100 to-violet-200 to-90%"
     >
       <div
-        className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4"
+        className="w-11/12 max-w-4xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4"
         id="header"
       >
         <div className="w-7/8 ">
-          <h2 className="text-2xl mb-5 font-Ovo">Hi there! I'm Esra Ayaz,</h2>
-          <p className="text-lg mx-auto mb-5">I am a self-made</p>
-          <h1 className="text-3xl sm:text-4xl lg:text-[55px] mb-10 font-Ovo">
+          <h2 className="text-2xl mb-5 ">Hi there! I'm Esra Ayaz,</h2>
+          <p className="text-lg mx-auto mb-5">a self-made</p>
+          <h1 className="text-3xl sm:text-4xl lg:text-[55px] mb-10">
             Front-End Web Developer
           </h1>
-          <h2 className="text-xl sm:text-2xl mb-10 font-Ovo">
-            📍 Inverness, UK.
-          </h2>
+          <h2 className="text-xl sm:text-2xl mb-10">📍 Inverness, UK.</h2>
         </div>
         <div>
           <motion.button
@@ -39,9 +37,8 @@ const Header = () => {
             }}
           >
             <span className="tracking-wide h-full w-full block relative linear-mask">
-              <a href="#contact">Contact Me</a>
+              <a href="#contact">Get in Touch</a>
             </span>
-            <Image src={assets.right_arrow} alt="arrow" className="w-3" />
             <span className="block absolute inset-0 rounded-md p-px linear-overlay" />
           </motion.button>
         </div>
