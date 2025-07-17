011c1afac30844df4583b86e3635f642d6c8c5d1
Esra Ayaz <essayaz@hotmail.com>
2025-07-17 16:48:08 +0200

    Navbar improved 💅

--- app/comps/Navbar.jsx
+++ app/comps/Navbar.jsx
@@ -9,11 +9,11 @@ const Navbar = () => {
   const sideMenuRef = useRef();
 
   const openMenu = () => {
-    sideMenuRef.current.style.transform = "translateX(-16rem)";
+    sideMenuRef.current.style.transform = "translateX(16rem)";
   };
 
   const closeMenu = () => {
-    sideMenuRef.current.style.transform = "translateX(16rem)";
+    sideMenuRef.current.style.transform = "translateX(-16rem)";
   };
 
   useEffect(() => {
@@ -29,59 +29,86 @@ const Navbar = () => {
   return (
     <div>
       <nav
-        className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-8 flex items-center justify-between z-50 ${
+        className={`w-full fixed px-5 md:px-6 lg:px-8 xl:px-[8%] py-8 flex items-center justify-between z-50 ${
           isScroll ? "bg-white/60 backdrop-blur-sm shadow-sm" : ""
         }`}
       >
-        <a href="#header">
-          <Image src={assets.logo} alt="logo" className="w-30 cursor-pointer" />
+        <button className="block md:hidden cursor-pointer" onClick={openMenu}>
+          <Image src={assets.menu_black} alt="menu-black" className="w-6" />
+        </button>
+        <a href="#header" className="md:flex-none cursor-pointer">
+          <Image src={assets.logo} alt="logo" className="w-32" />
         </a>
         <ul
-          className={`hidden md:flex items-center md:gap-8 lg:gap-20 rounded-full px-12 py-3 ${
+          className={`hidden md:flex items-center gap-4 lg:gap-8 xl:gap-12 rounded-full px-6 lg:px-8 xl:px-12 py-3 ${
             isScroll ? "" : "bg-white/90 shadow-md"
-          }  mr-22`}
+          }  mx-4`}
         >
           <li>
-            <a href="#header">Home</a>
+            <a
+              className="hover:text-violet-800 active:text-violet-800 text-sm mx-3 lg:text-base"
+              href="#header"
+            >
+              Home
+            </a>
           </li>
           <li>
-            <a href="#about">About Me</a>
+            <a
+              className="hover:text-violet-800 active:text-violet-800 text-sm mx-3 lg:text-base"
+              href="#about"
+            >
+              About Me
+            </a>
           </li>
           <li>
-            <a href="#project">Projects</a>
+            <a
+              className="hover:text-violet-800 active:text-violet-800 text-sm mx-3 lg:text-base"
+              href="#project"
+            >
+              Projects
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact Me</a>
+            <a
+              className="hover:text-violet-800 active:text-violet-800 text-sm mx-3 lg:text-base"
+              href="#blog"
+            >
+              Blog
+            </a>
+          </li>
+          <li>
+            <a
+              className="hover:text-violet-800 active:text-violet-800 text-sm mx-3 lg:text-base"
+              href="#contact"
+            >
+              Contact Me
+            </a>
           </li>
         </ul>
 
-        <div className="flex items-center">
+        <div className="flex items-center gap-2">
           <button className="hidden md:flex cursor-pointer">
-            <Image src={assets.moon_icon} alt="moon" className="w-6" />
-          </button>
-
-          <button className="block md:hidden" onClick={openMenu}>
-            <Image src={assets.menu_black} alt="menu-black" className="w-6" />
+            <Image src={assets.moon_icon} alt="moon" className="w-7" />
           </button>
         </div>
 
         {/*-- --------- Mobile Menu --------- --*/}
         <ul
           ref={sideMenuRef}
-          className="flex md:hidden flex-col gap-5 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-fuchsia-50 transition duration-500"
+          className="flex md:hidden flex-col gap-5 py-20 px-10 fixed -left-64 top-0 bottom-0 w-64 z-50 h-screen bg-fuchsia-50 transition duration-500"
         >
           <div className="absolute right-6 top-6" onClick={closeMenu}>
             <Image
               src={assets.close_black}
               alt="close-icon"
-              className="w-5 mt-3 cursor-pointer"
+              className="w-8 mt-3 cursor-pointer"
             />
           </div>
           <button>
             <Image
               src={assets.moon_icon}
               alt="moon"
-              className="md:hidden w-6 cursor-pointer"
+              className="md:hidden w-8 mb-4 cursor-pointer"
             />
           </button>
           <li>
@@ -97,6 +124,11 @@ const Navbar = () => {
           <li>
             <a onClick={closeMenu} href="#project">
               💻 Projects
+            </a>
+          </li>
+          <li>
+            <a onClick={closeMenu} href="#blog">
+              🌱 Blog
             </a>
           </li>
           <li>
