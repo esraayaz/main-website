53fb25d3bc98d56d8fbc13fb8f5b5f0356a738cd
Esra Ayaz <essayaz@hotmail.com>
2025-07-19 01:25:02 +0200

    Footer and Toaster added 💻

--- app/page.js
+++ app/page.js
@@ -3,15 +3,19 @@ import Header from "./header/page";
 import About from "./about/page";
 import Proje from "./proje/page";
 import Contact from "./contact/page";
+import Footer from "./footer/page";
+import { ToastContainer } from "react-toastify";
 
 export default function Home() {
   return (
     <>
+      <ToastContainer />
       <Navbar />
       <Header />
       <About />
       <Proje />
       <Contact />
+      <Footer />
     </>
   );
 }
